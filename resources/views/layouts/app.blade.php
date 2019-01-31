da7325156d1ba9a2c24f0ccacd7a76e6ffe775c0
vasar <panwenmingchn@hotmail.com>
2019-02-01 00:13:58 +0800

    edit index

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,11 +5,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- 上述3个meta标签*必须*放在最前面，任何其他内容都*必须*跟随其后！ -->
-    <meta name="description" content="">
-    <meta name="author" content="">
+    <meta name="keywords" content="blog,document,英语,在线工具,程序员,工具,开发人员工具,小工具,API查询,文档,推荐软件,下载">
+    <meta name="description" content="快速查阅,快乐工作">
+    <meta name="author" content="ohmytool">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="icon" href="../../favicon.ico">
+    <link rel="shortcut icon" href="/favicon.ico">
 
     <title>@yield('title', 'Ohmytool') - 工具资源</title>
 
