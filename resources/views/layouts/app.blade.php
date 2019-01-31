5a17b43f41a48a703b3e1e82d4d63cfb6c97c469
vasar <panwenmingchn@hotmail.com>
2019-02-01 00:37:50 +0800

    add google adsence coad

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -83,6 +83,13 @@
         }
     });
 </script>
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script>
+    (adsbygoogle = window.adsbygoogle || []).push({
+        google_ad_client: "ca-pub-6010623452037264",
+        enable_page_level_ads: true
+    });
+</script>
 @yield('script')
 </body>
 </html>
