5e480aefc858461c628f62794a8307c55b42b1c4
vasar <panwenmingchn@hotmail.com>
2019-02-01 22:20:19 +0800

    edit index page

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -20,9 +20,9 @@ class PageController extends Controller
         $apiDocs = Tool::query()->where('category_id', $cate_api_doc_id)->orderByDesc('click_count')->limit(10)->get();
         $manuals = Tool::query()->where('category_id', $cate_manual_id)->orderByDesc('click_count')->limit(10)->get();
         $blogs = Tool::query()->where('category_id', $cate_blog_id)->orderByDesc('click_count')->limit(10)->get();
-        $tools = Tool::query()->where('category_id', $cate_tool_id)->orderByDesc('click_count')->limit(20)->get();
-        $englishs = Tool::query()->where('category_id', $cate_english_id)->orderByDesc('click_count')->limit(20)->get();
-        $softwares = Tool::query()->where('category_id', $cate_software_id)->orderByDesc('click_count')->limit(20)->get();
+        $tools = Tool::query()->where('category_id', $cate_tool_id)->orderByDesc('click_count')->limit(32)->get();
+        $englishs = Tool::query()->where('category_id', $cate_english_id)->orderByDesc('click_count')->limit(32)->get();
+        $softwares = Tool::query()->where('category_id', $cate_software_id)->orderByDesc('click_count')->limit(32)->get();
 
 
         return view("page.index", compact('apiDocs', 'blogs', 'tools', 'englishs', 'softwares', 'manuals'));
