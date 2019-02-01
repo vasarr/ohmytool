c5126612559df703a748e740334eb695a94a2d19
vasar <panwenmingchn@hotmail.com>
2019-02-01 15:45:49 +0800

    add contact way

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,5 +1,4 @@
 <div class="clearfix"></div>
 <footer class="footer">
-    <p>&copy; 2019 ohmytool.</p>
+    <p>&copy; 2019 ohmytool. <a href="mailto:panwenmingchn@hotmail.com">contact</a></p>
 </footer>
-
